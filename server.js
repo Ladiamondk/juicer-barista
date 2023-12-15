c7570bd66003317d44cfeb83a2a5b9f044e4049f
Ladiamondk <108835981+Ladiamondk@users.noreply.github.com>
2023-12-15 04:09:41 +0000

    none

--- server.js
+++ server.js
@@ -12,7 +12,36 @@ app.use(express.urlencoded({extended: true}));
 const PORT = process.env.PORT || 3000;
 const CONNECTION = process.env.CONNECTION;
 
+//Define schema for your data
+const userSchema = new mongoose.Schema({
+    firstname: String,
+    lastname: String,
+    email: String
+});
 
+const User = mongoose.model('User', userSchema);
+app.get('/', (req, res) => {
+    res.send('Hello, this is the root route!');
+  });
+  
+//Handle form submission
+app.post('/submit', async(req, res) => {
+    try {
+        const newUser = new User({
+            firstname: req.body.fname,
+            lastname: req.body.lname,
+            email: req.body.email
+        });
+
+        //Saving user to the database
+        await newUser.save();
+
+        res.status(200).send('Form submitted successfully');
+    } catch(error){
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
+})
 
 const start = async() => {
     try{
